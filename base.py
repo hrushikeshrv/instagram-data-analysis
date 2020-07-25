365dc3a54986ec3ea7e20d4d5a54c2b6bffaefe7
Hrushikesh Vaidya <64729027+hrushikeshrv@users.noreply.github.com>
2020-07-25 17:41:19 +0530

    Update base.py

--- base.py
+++ base.py
@@ -9,13 +9,13 @@ import seaborn as sns
 from instagram_utils import find_person, print_conversation, cleaned, frequency_analysis, frequency_plot
 
 #Set my_username as your instagram username
-my_username = 'hrushikeshrv'
+my_username = ''
 #Pass pd.read_json() the full path to the messages.json file in your instagram data
 #For example - C://Users/.../Instagram_Data-20200629T20118Z-001/Data/username_date_part_1/messages.json
 dms = pd.read_json('C:/Users/Hrush/Downloads/hrushikeshrv_20200722_part_1/messages.json')
 
 #Enter the account name you want to analyze your history with
-person = 'amrunikothadia'
+person = ''
 
 posts_sent, posts_recieved, photos_sent, photos_recieved = print_conversation(person, my_username, dms, True)
 frequency_plot(person, dms, 3)
